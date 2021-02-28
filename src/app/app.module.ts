09ec52ec1f3ed25b8a635a2da935552296f2e5d1
Vyctor <dev.vyctor@gmail.com>
2021-02-28 20:50:27 -0300

    feat: fimplemented binToDec and decToBin functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +9,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
+// Components Imports
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { ConversorComponent } from './components/conversor/conversor.component';
 
@@ -24,7 +31,13 @@ import { ConversorComponent } from './components/conversor/conversor.component';
     BrowserAnimationsModule,
     MatMenuModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/conversor/conversor.component.ts
+++ src/app/components/conversor/conversor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-conversor',
   templateUrl: './conversor.component.html',
@@ -7,9 +7,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConversorComponent implements OnInit {
 
-  constructor() { }
+  public formConversao: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.formConversao = this.formBuilder.group({
+      baseNumerica: [''],
+      valor: ['']
+    });
   }
 
+  ngOnInit(): void {
+    this.formConversao.valueChanges.subscribe(data => {
+      if (data.baseNumerica && data.valor) {
+        this.convert(data.baseNumerica, data.valor);
+      }
+    });
+  }
+
+  convert(base: string, value: string): void {
+    switch (base) {
+      case 'decimal':
+        this.decToBin(value);
+        console.log(this.decToBin(value));
+        break;
+      case 'binario':
+        this.binToDec(value);
+        console.log(this.binToDec(value));
+        break;
+    }
+  }
+
+  binToDec(value: string): number {
+    let result = 0;
+    const binaryValue = (value.split('').reverse().map(v => parseInt(v, undefined)));
+    binaryValue.forEach((ditig, index) => {
+      console.log(`${ditig} * ${ditig} ˆ ${index}`);
+      result += ditig * (Math.pow(2, index));
+    });
+    return result;
+  }
+
+  decToBin(value: string): number {
+    const result: number[] = [];
+    let decimalValue = parseInt(value, undefined);
+
+    while (decimalValue > 0) {
+      if (decimalValue % 2 === 0) {
+        result.push(0);
+        decimalValue = decimalValue / 2;
+      }
+      if (decimalValue % 2 === 1) {
+        result.push(1);
+        decimalValue = (decimalValue - 1) / 2;
+      }
+    }
+    return parseInt(result.reverse().join(''), undefined);
+  }
 }
